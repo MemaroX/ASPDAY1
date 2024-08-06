32aa6e9fd44c9d94b57d685b45a106e98563acbb
Maher <Maher@DESKTOP-US15PS7>
2024-08-06 12:42:49 +0300

    Add project files.

---
+++ ASPDAY1/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using ASPDAY1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDAY1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Ahmed", Age = 20, Email = "ahmed@example.com", PhoneNumber = "1234567890", Address = "menouf" },
+                new Student { Id = 2, Name = "Mohamed", Age = 22, Email = "mohamed@example.com", PhoneNumber = "9876543210", Address = "El minya" },
+                new Student { Id = 3, Name = "George", Age = 21, Email = "george@example.com", PhoneNumber = "123123123132", Address = "Cairo" },
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ ASPDAY1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace ASPDAY1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        //add 3 more attributes to student model
+        //add 5 students to the list
+        //map these attributes in controller to view
+        //use bootstrap to enhance ui
+    }
+}
