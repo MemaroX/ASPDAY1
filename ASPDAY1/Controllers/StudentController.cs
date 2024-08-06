16e83e16bc37f0fe8d58f97fd142a5c231dd7239
MemaroX <boy15maher50@gmail.com>
2024-08-06 13:29:56 +0300

    Adding Students

--- ASPDAY1/Controllers/StudentController.cs
+++ ASPDAY1/Controllers/StudentController.cs
@@ -13,6 +13,8 @@ namespace ASPDAY1.Controllers
                 new Student { Id = 1, Name = "Ahmed", Age = 20, Email = "ahmed@example.com", PhoneNumber = "1234567890", Address = "menouf" },
                 new Student { Id = 2, Name = "Mohamed", Age = 22, Email = "mohamed@example.com", PhoneNumber = "9876543210", Address = "El minya" },
                 new Student { Id = 3, Name = "George", Age = 21, Email = "george@example.com", PhoneNumber = "123123123132", Address = "Cairo" },
+                new Student { Id = 4, Name = "Mohsen", Age = 23, Email = "mohsen@example.com", PhoneNumber = "13231231231", Address = "Alex" },
+                new Student { Id = 5, Name = "Kamel", Age = 20, Email = "kamel@example.com", PhoneNumber = "431412312312", Address = "Bani suef" }
             };
 
             return View(students);
